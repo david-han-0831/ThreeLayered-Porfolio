d4b55bee6138f4bbb89b257f7a612aba6d314f76
David <david@colon-b.com>
2025-05-26 21:50:54 +0900

    fix: 메타데이터 설정

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,35 @@ import Hero from "@/components/Hero"
 import MotionGraphics from "@/components/MotionGraphics"
 import AboutUs from "@/components/AboutUs"
 import Project from "@/components/Project"
+import { Metadata } from "next"
+// ✅ 메타데이터 설정 (절대경로로 작성해야 카카오가 인식 가능)
+export const metadata: Metadata = {
+  title: "ThreeLayered",
+  description: "ThreeLayered - 웹 개발자 포트폴리오",
+  openGraph: {
+    title: 'ThreeLayered',
+    description: '디자인부터 개발까지, 브랜드를 완성하는 크리에이티브 스타트업 – ThreeLayered',
+    url: 'https://www.three-layered.com/',
+    siteName: 'ThreeLayered',
+    images: [
+      {
+        url: 'https://www.three-layered.com/logo/logo.svg', // 절대경로!
+        width: 1200,
+        height: 630,
+        alt: 'ThreeLayered Logo',
+      },
+    ],
+    locale: 'ko_KR',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'ThreeLayered',
+    description: 'ThreeLayered - 웹 개발자 포트폴리오',
+    images: ['https://www.three-layered.com/logo/logo.svg'],
+  },
+}
+
 export default function Home() {
   return (
     <main className="min-h-screen">
